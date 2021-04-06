cc66a281e07f49cdc0872c65476555ce77f4c650
Lazytangent <pm123633@gmail.com>
2021-04-06 17:55:53 -0500

    Clean up app.js

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,13 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
