af0eea5b4f5bcda3907229dd6446e72d25a80ad7
Lazytangent <pm123633@gmail.com>
2021-04-06 18:25:41 -0500

    More app set up

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,37 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const helmet = require('helmet');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const helmet = require("helmet");
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(helmet({
-  contentSecurityPolicy: false,
-}));
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+
+app.use((_req, _res, next) => {
+  const err = new Error('The requested resource couldn\'t be found.');
+  err.title = 'Resource Not Found';
+  err.errors = ['The requested resource couldn\'t be found.'];
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: err.stack,
+  });
+});
 
 module.exports = app;
