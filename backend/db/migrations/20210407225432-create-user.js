256f5ae4f0767d7019ea6e6b9ed018309f23ea71
Lazytangent <pm123633@gmail.com>
2021-04-07 18:08:06 -0500

    Update migration

--- backend/db/migrations/20210407225432-create-user.js
+++ backend/db/migrations/20210407225432-create-user.js
@@ -25,12 +25,12 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW,
+        defaultValue: Sequelize.fn('now'),
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW,
+        defaultValue: Sequelize.fn('now'),
       }
     });
   },
