f337d6bdf4f51d9cc5f620d3f20b80d3b1efb401
Lazytangent <pm123633@gmail.com>
2021-04-07 09:47:29 -0500

    Set up Users slice of state

---
+++ frontend/src/store/users.js
@@ -0,0 +1,16 @@
+// Define Action Types as Constants
+
+// Define Action Creators
+
+// Define Thunks
+
+const initialState = {};
+
+const UsersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default UsersReducer;
