5b948f8d58dba4a6b9432d7333fae5267c737804
Lazytangent <pm123633@gmail.com>
2021-04-07 14:48:10 -0500

    Conditionally render UserRow based on queryString

--- frontend/src/components/UserRow/UserRow.js
+++ frontend/src/components/UserRow/UserRow.js
@@ -1,4 +1,12 @@
+import { useSelector } from 'react-redux';
+
 const UserRow = ({ user }) => {
+  const queryString = useSelector((state) => state.search.queryString);
+
+  if (!(user.name.includes(queryString) || user.email.includes(queryString) || String(user.id).includes(queryString))) {
+    return null;
+  }
+
   return (
     <tr>
       <td>{user.id}</td>
