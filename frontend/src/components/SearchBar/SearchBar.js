12457a65a194113a4478d50b642016d0260efdbd
Lazytangent <pm123633@gmail.com>
2021-04-07 13:58:20 -0500

    Use Redux in SearchBar

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -1,6 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { setSearch } from '../../store/search';
+
 const SearchBar = () => {
+  const dispatch = useDispatch();
+  const queryString = useSelector((state) => state.search.queryString);
+
+  const updateSearch = (e) => {
+    dispatch(setSearch(e.target.value))
+  };
+
   return (
-    <h3>Placeholder for SearchBar</h3>
+    <>
+      <h3>Placeholder for SearchBar</h3>
+      <input type="search" value={queryString} onChange={updateSearch} />
+    </>
   );
 };
 
