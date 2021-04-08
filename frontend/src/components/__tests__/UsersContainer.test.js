b0a3d3a404ccd09b708c422691cfc4b3c08826ae
Lazytangent <pm123633@gmail.com>
2021-04-08 17:58:46 -0500

    Working UsersContainer tests

---
+++ frontend/src/components/__tests__/UsersContainer.test.js
@@ -0,0 +1,43 @@
+import { render, screen } from "../../utils/test-utils";
+import fetchMock from "fetch-mock";
+
+import UsersContainer from "../UsersContainer";
+
+const users = [
+  {
+    id: 1,
+    name: "Test User",
+    email: "test@aa.io",
+  },
+  {
+    id: 2,
+    name: "UserTest",
+    email: "test@user.io",
+  },
+];
+
+describe("The UsersContainer component", () => {
+  describe("renders", () => {
+    beforeEach(() => {
+      fetchMock.getOnce("/api/users", {
+        body: users,
+        headers: { "Content-Type": "application/json" },
+      });
+
+      render(
+        <UsersContainer />,
+      );
+    });
+
+    afterEach(() => {
+      fetchMock.restore();
+    });
+
+    test("the test users", () => {
+      const userOneId = screen.getByText("1");
+      const userTwoId = screen.getByText("2");
+      expect(userOneId).toHaveTextContent("1");
+      expect(userTwoId).toHaveTextContent("2");
+    });
+  });
+});
