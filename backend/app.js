67db0909af7c6198057c4189954d66801210bcbf
Lazytangent <pm123633@gmail.com>
2021-04-08 15:38:31 -0500

    Revert

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const { User } = require("./db/models");
 
 const app = express();
 
