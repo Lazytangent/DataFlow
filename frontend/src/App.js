7819430da0a893e11e316554cc1ef76a657994c5
Lazytangent <pm123633@gmail.com>
2021-04-09 12:01:51 -0500

    Move heading into main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
-import styles from './App.module.css';
 import MainPage from './pages/MainPage';
 
 const App = () => {
   return (
     <>
-      <h2 className={styles.heading}>Search for Users</h2>
       <MainPage />
     </>
   );
