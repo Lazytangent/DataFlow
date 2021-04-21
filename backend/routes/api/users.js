6258753ded2a9d5925a54bd8ce58e9a8267a694a
Lazytangent <pm123633@gmail.com>
2021-04-21 11:07:07 -0500

    Add User import for users routes

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 
+const { User } = require('../../db/models');
 const router = express.Router();
 
 // Or create the API route here...
